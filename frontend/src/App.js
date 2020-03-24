b03bd3ac0ed7e35bf98f3d1cd7d6e518e4b7eb95
Leonardo de Jesus Silva <LJS@Leonardos-MBP.home>
2020-03-24 23:22:43 +0000

    Application Front End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello OnmiStack
+          Hello OnmiStack Application Front-End
         </p>
         <a
           className="App-link"
