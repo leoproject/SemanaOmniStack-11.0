718476773dd58c54e7c56bb159283d3224eaa120
Leonardo de Jesus Silva <LJS@Leonardos-MBP.home>
2020-03-25 20:10:22 +0000

    Construido a pagina de cadastro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
-import React, {useState} from 'react';
-import Header from './Header';
+import './global.css';
+import React from 'react';
+//import Header from './Header';
+import Logon from './pages/Logon';
 
 function App() {
-  let [counter,setCounter] = useState(0);
-  // retorna dois array 
-  // Array [valor, funçãodeAtualização] 
-
-    function increment(){
-        setCounter(counter+1);
-    }
-
     return (
-        <div>
-        <Header>Counter: {counter}</Header>
-        <button onClick={increment}>Increment</button>
-        </div>
+        <Logon/>
     );
 }
 
